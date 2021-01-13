56a65a72637ab3a2cbe1e41812a81029b520d21e
laureen <laureen262@hotmail.fr>
2021-01-13 20:11:30 +0100

    create entity user

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route ("", name="profile")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'website' => 'Laureen Dumas',
+            ]);
+    }
+}
